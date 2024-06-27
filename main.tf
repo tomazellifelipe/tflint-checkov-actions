d7f675cc805c45a9fbd81ac3c3850329daaf1a58
Felipe Tomazelli <fefetl08@gmail.com>
2024-06-26 21:53:04 -0300

    feat: disable public access and create lifecycle rule

--- main.tf
+++ main.tf
@@ -15,3 +15,24 @@ provider "aws" {
 resource "aws_s3_bucket" "tflint_checkov" {
   bucket_prefix = "tflint-checkov"
 }
+
+resource "aws_s3_bucket_lifecycle_configuration" "abort_incomplete_multipart" {
+  bucket = aws_s3_bucket.tflint_checkov.id
+  rule {
+    id     = "abort-incomplete-multipart-uploads"
+    status = "Enabled"
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
+  }
+
+}
+
+resource "aws_s3_bucket_public_access_block" "block_all" {
+  bucket                  = aws_s3_bucket.tflint_checkov.id
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
+
